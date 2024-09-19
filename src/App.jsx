a63925982e30fb5db4dd8de0918542ef5970d8e3
Abdul Hamid <hamidkhattak469@gmail.com>
2024-09-19 20:39:32 +0500

    Rename images name of Abdul Hamid. Work on Hero Component pub image of avator and profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
 
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </>
   )
 }
 
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,6 +1,32 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="pt-28 lg:pt-36">
+      <div className="container lg:grid lg:grid-cols-2 items-center lg:gap-10">
+        <div>
+          <div className="">
+            <figure className="">
+              <img src="./imgs/avatar-1.jpg" alt="Abdul Hamid" width={40} height={40} className="img-cover" />
+            </figure>
+            <div className="">
+              <span className="">
+                <span className=""></span>
+              </span>
+              Available for work
+            </div>
+          </div>
+          <h2 className="">Building Scalable Modern Websites for the Future</h2>
+          <div>
+            ButtonPrimary
+            ButtonOutline
+          </div>
+        </div>
+        <div className="">
+          <figure>
+            <img src="./imgs/Abdul-Hamid.png" alt="Abdul-Hamid-Frontend-Developer" width={450} height={450} className="" />
+          </figure>
+        </div>
+      </div>
+    </section>
   )
 }
 
