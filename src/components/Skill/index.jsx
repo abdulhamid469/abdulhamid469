2c1dba4aab9a64f0ca569b8b3f12fbb7f7a4242a
Abdul Hamid <hamidkhattak469@gmail.com>
2024-10-12 23:41:09 +0500

    Issue resloved in SkillCard component

--- src/components/Skill/index.jsx
+++ src/components/Skill/index.jsx
@@ -49,7 +49,7 @@ const Skill = () => {
       <div className="container">
         <h2 className="headline-2">Essential Tools I use</h2>
         <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch]">Discover the powerful tools and technologies I use to create exceptional, high-performing websites & applications.</p>
-        <div className="">
+        <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
           {
             skillItem.map(({ imgSrc, label, desc }, key) => (
               <SkillCard key={key} imgSrc={imgSrc} label={label} desc={desc} />
