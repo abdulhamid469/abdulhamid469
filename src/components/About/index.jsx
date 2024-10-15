b8b637b14a3a4b930859eb59932b7b65d0938f33
Abdul Hamid <hamidkhattak469@gmail.com>
2024-10-15 15:06:00 +0500

    Create Work and ProjectCard components and some changes in About component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -14,7 +14,7 @@ const About = () => {
     <section id="about" className="section">
       <div className="container">
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
-          <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">Welcome! I&apos;m Henry, a professional web developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise. I transform your vision into digital masterpiece that excels in both appearance and performance.</p>
+          <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">Welcome! I&apos;m <span className="text-orange-400">Abdul Hamid</span>, a Professional Full Stack Developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise. I transform your vision into digital masterpiece that excels in both appearance and performance.</p>
           <div className="flex flex-wrap items-center gap-4 md:gap-7">
             {
               aboutItems.map(({ label, number }, key) => (
