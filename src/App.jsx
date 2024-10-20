567516f72538947789c9ea152ca8b2981228a15f
Abdul Hamid <hamidkhattak469@gmail.com>
2024-10-20 21:40:58 +0500

    Review and ReviewCard component is completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skill from './components/Skill';
 import Work from './components/Work';
+import Review from './components/Review';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <About />
         <Skill />
         <Work />
+        <Review />
       </main>
     </>
   )
