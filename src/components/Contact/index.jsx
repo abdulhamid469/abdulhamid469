3b009b7bc3d82fd9f7297a88da994149636a905a
Abdul Hamid <hamidkhattak469@gmail.com>
2024-10-26 23:54:12 +0500

    Some of desing is complated in Contact Component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -31,21 +31,21 @@ const socialLinks = [
 const Contact = () => {
   return (
     <section id="contact" className="section">
-      <div className="container">
-        <div className="">
-          <h2 className="headline-2">Contact me for collaboration</h2>
-          <p className="">Reach out today to discuss your project needs and start collaborating on something amazing!</p>
-          <div className="">
+      <div className="container lg:grid lg:grid-cols-2 lg:items-stretch">
+        <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
+          <h2 className="headline-2 lg:max-w-[12ch]">Contact me for collaboration</h2>
+          <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch]">Reach out today to discuss your project needs and start collaborating on something amazing!</p>
+          <div className="flex items-center gap-2 mt-auto">
             {
               socialLinks.map(({ href, icon }, key) => (
-                <a key={key} href={href} target="_blank">{icon}</a>
+                <a key={key} href={href} target="_blank" className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80">{icon}</a>
               ))
             }
           </div>
         </div>
-        <form action="" method="POST" className="">
-          <div className="">
-            <div className="">
+        <form action="" method="POST" className="xl:pl-10 2xl:pl-20">
+          <div className="md:grid md:items-center md:grid-cols-2 md:gap-2">
+            <div className="mb-4">
               <label htmlFor="name" className="label">Name</label>
               <input type="text" name="name" id="name" autoComplete="name" placeholder="Jone Deo" className="text-field" required />
             </div>
@@ -55,7 +55,7 @@ const Contact = () => {
             </div>
           </div>
           <div className="">
-            <label htmlFor="message">Message</label>
+            <label htmlFor="message" className="label">Message</label>
             <textarea name="message" id="message" placeholder="Hi! Enter Your Message" className="text-field" required></textarea>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
